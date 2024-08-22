7f26778385747d5f0553ed2dfe748abd94f33b99
Mariam-Elkhelawy <mariamelkhelawy2001@gmail.com>
2024-08-23 01:04:14 +0300

    create tabs

---
+++ lib/features/gold/presentation/pages/gold_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GoldTab extends StatelessWidget {
+  const GoldTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
