9775dfd9322ca7a103b0522965107624f4490210
Mariam-Elkhelawy <mariamelkhelawy2001@gmail.com>
2024-08-25 15:58:28 +0300

    finish task

--- test/currency_test.dart
+++ test/currency_test.dart
@@ -1,34 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hne_futures_task/core/errors/failures.dart';
-import 'package:mockito/mockito.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hne_futures_task/features/currency/presentation/bloc/currency_bloc.dart';
-import 'package:hne_futures_task/features/currency/presentation/pages/currency_tab.dart';
-import 'package:hne_futures_task/features/currency/data/models/CurrencyModel.dart';
 import 'package:hne_futures_task/core/enums/enums.dart';
+import 'package:hne_futures_task/features/currency/presentation/pages/currency_tab.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:hne_futures_task/features/currency/presentation/bloc/currency_bloc.dart';
+import 'package:hne_futures_task/features/currency/data/models/CurrencyModel.dart';
 
 class MockCurrencyBloc extends Mock implements CurrencyBloc {}
 
 void main() {
-  late MockCurrencyBloc mockCurrencyBloc;
+  late CurrencyBloc mockCurrencyBloc;
 
   setUp(() {
     mockCurrencyBloc = MockCurrencyBloc();
   });
 
-  testWidgets(
-      'CurrencyTab displays currency rate for EGP when state is success',
-      (WidgetTester tester) async {
-    final currencyModel = CurrencyModel(
-      base: 'EUR',
-      rates: Rates(egp: 19.05),
-    );
-
-    when(mockCurrencyBloc.state).thenReturn(
+  testWidgets('displays exchange rate between EUR and EGP', (WidgetTester tester) async {
+    when(() => mockCurrencyBloc.state).thenReturn(
       CurrencyState(
         status: ScreenStatus.success,
-        currencyModel: currencyModel,
+        currencyModel: CurrencyModel(
+          rates: Rates(egp: 54.60),
+        ),
       ),
     );
 
@@ -41,52 +35,9 @@ void main() {
       ),
     );
 
-    await tester.pump();
+    await tester.pumpAndSettle();
 
-    expect(find.text('EGP'), findsOneWidget);
-    expect(find.text('19.05'), findsOneWidget);
+    expect(find.text('1 EUR = 54.60 EGP'), findsOneWidget);
   });
 
-  testWidgets('CurrencyTab shows error message when state is failure',
-      (WidgetTester tester) async {
-    when(mockCurrencyBloc.state).thenReturn(
-      CurrencyState(
-        status: ScreenStatus.failure,
-        failures: RemoteFailures('Failed to fetch data'),
-      ),
-    );
-
-    await tester.pumpWidget(
-      MaterialApp(
-        home: BlocProvider<CurrencyBloc>.value(
-          value: mockCurrencyBloc,
-          child: const CurrencyTab(),
-        ),
-      ),
-    );
-
-    await tester.pump();
-
-    expect(find.text('Failed to fetch data'), findsOneWidget);
-  });
-
-  testWidgets('CurrencyTab shows loading indicator when state is loading',
-      (WidgetTester tester) async {
-    when(mockCurrencyBloc.state).thenReturn(
-      const CurrencyState(status: ScreenStatus.loading),
-    );
-
-    await tester.pumpWidget(
-      MaterialApp(
-        home: BlocProvider<CurrencyBloc>.value(
-          value: mockCurrencyBloc,
-          child: const CurrencyTab(),
-        ),
-      ),
-    );
-
-    await tester.pump();
-
-    expect(find.byType(CircularProgressIndicator), findsOneWidget);
-  });
 }
--- test/gold_test.dart
+++ test/gold_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:hne_futures_task/core/errors/failures.dart';
-import 'package:mockito/mockito.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hne_futures_task/core/enums/enums.dart';
+import 'package:hne_futures_task/core/utils/app_strings.dart';
+import 'package:hne_futures_task/features/gold/data/models/GoldModel.dart';
 import 'package:hne_futures_task/features/gold/presentation/bloc/gold_bloc.dart';
 import 'package:hne_futures_task/features/gold/presentation/pages/gold_tab.dart';
-import 'package:hne_futures_task/features/gold/data/models/GoldModel.dart';
-import 'package:hne_futures_task/core/enums/enums.dart';
+import 'package:mocktail/mocktail.dart';
 
 class MockGoldBloc extends Mock implements GoldBloc {}
 
@@ -17,75 +17,42 @@ void main() {
     mockGoldBloc = MockGoldBloc();
   });
 
-  testWidgets('GoldTab displays gold prices when state is success',
-      (WidgetTester tester) async {
-    final goldModel = GoldModel(
-      priceGram24k: 3000,
-      priceGram22k: 2700,
-      priceGram21k: 2600,
-      priceGram20k: 2500,
-      priceGram18k: 2300,
-      priceGram16k: 2100,
-      priceGram14k: 1900,
-      priceGram10k: 1500,
+  testWidgets('GoldTab displays gold prices when state is success', (WidgetTester tester) async {
+    when(() => mockGoldBloc.state).thenReturn(
+      GoldState(
+        status: ScreenStatus.success,
+        goldModel: GoldModel(
+          priceGram24k: 3936.654,
+          priceGram22k: 3608.5995,
+          priceGram21k: 3444.5723,
+          priceGram20k: 3280.545,
+          priceGram18k: 2952.4905,
+          priceGram16k: 2624.436,
+          priceGram14k: 2296.3815,
+          priceGram10k: 1640.2725,
+        ),
+      ),
     );
-
-    when(mockGoldBloc.state).thenReturn(
-        GoldState(status: ScreenStatus.success, goldModel: goldModel));
 
     await tester.pumpWidget(
       MaterialApp(
-        home: BlocProvider<GoldBloc>.value(
-          value: mockGoldBloc,
+        home: BlocProvider<GoldBloc>(
+          create: (context) => mockGoldBloc,
           child: const GoldTab(),
         ),
       ),
     );
 
-    await tester.pump();
-
-    expect(find.text('24k'), findsOneWidget);
-    expect(find.text('3000'), findsOneWidget);
-    expect(find.text('18k'), findsOneWidget);
-    expect(find.text('2300'), findsOneWidget);
+    await tester.pumpAndSettle();
+    expect(find.text('24K ${AppStrings.gold} ${AppStrings.priceCapital} 3936.65 ${AppStrings.eg}'), findsOneWidget);
+    expect(find.text('22K ${AppStrings.gold} ${AppStrings.priceCapital} 3608.60 ${AppStrings.eg}'), findsOneWidget);
+    expect(find.text('21K ${AppStrings.gold} ${AppStrings.priceCapital} 3444.57 ${AppStrings.eg}'), findsOneWidget);
+    expect(find.text('20K ${AppStrings.gold} ${AppStrings.priceCapital} 3280.55 ${AppStrings.eg}'), findsOneWidget);
+    expect(find.text('18K ${AppStrings.gold} ${AppStrings.priceCapital} 2952.49 ${AppStrings.eg}'), findsOneWidget);
+    expect(find.text('16K ${AppStrings.gold} ${AppStrings.priceCapital} 2624.44 ${AppStrings.eg}'), findsOneWidget);
+    expect(find.text('14K ${AppStrings.gold} ${AppStrings.priceCapital} 2296.38 ${AppStrings.eg}'), findsOneWidget);
+    expect(find.text('10K ${AppStrings.gold} ${AppStrings.priceCapital} 1640.27 ${AppStrings.eg}'), findsOneWidget);
   });
 
-  testWidgets('GoldTab shows error message when state is failure',
-      (WidgetTester tester) async {
-    when(mockGoldBloc.state).thenReturn(GoldState(
-        status: ScreenStatus.failure,
-        failures: RemoteFailures('Failed to fetch data')));
 
-    await tester.pumpWidget(
-      MaterialApp(
-        home: BlocProvider<GoldBloc>.value(
-          value: mockGoldBloc,
-          child: const GoldTab(),
-        ),
-      ),
-    );
-
-    await tester.pump();
-
-    expect(find.text('Failed to fetch data'), findsOneWidget);
-  });
-
-  testWidgets('GoldTab shows loading indicator when state is loading',
-      (WidgetTester tester) async {
-    when(mockGoldBloc.state)
-        .thenReturn(const GoldState(status: ScreenStatus.loading));
-
-    await tester.pumpWidget(
-      MaterialApp(
-        home: BlocProvider<GoldBloc>.value(
-          value: mockGoldBloc,
-          child: const GoldTab(),
-        ),
-      ),
-    );
-
-    await tester.pump();
-
-    expect(find.byType(CircularProgressIndicator), findsOneWidget);
-  });
 }
